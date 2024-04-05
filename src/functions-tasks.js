4778675c8a7111b13550125094326695919bcc99
Naya252 <naya252naya@gmail.com>
2024-04-06 00:21:28 +0300

    feat: add solution to memoize

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -113,8 +113,19 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cache = new Map();
+
+  return (...args) => {
+    const key = JSON.stringify(args);
+
+    if (!cache.has(key)) {
+      const result = func(...args);
+      cache.set(key, result);
+    }
+
+    return cache.get(key);
+  };
 }
 
 /**
