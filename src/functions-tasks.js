7a017af1bcd67fcecdbc53414611ad7785c322fd
Naya252 <naya252naya@gmail.com>
2024-04-06 00:02:51 +0300

    feat: add solution to getFunctionBody

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -32,8 +32,14 @@ function getCurrentFunctionName() {
  *   getFunctionBody(hiHello) => "function hiHello() { console.log('hello world'); }"
  *
  */
-function getFunctionBody(/* func */) {
-  throw new Error('Not implemented');
+function getFunctionBody(func) {
+  if (typeof func !== 'function') {
+    return '';
+  }
+
+  const funcString = func.toString();
+
+  return funcString;
 }
 
 /**
