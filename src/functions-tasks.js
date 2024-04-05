956d320ebdf668484d09b78c8ceed38756a784ed
Naya252 <naya252naya@gmail.com>
2024-04-06 00:25:11 +0300

    feat: add solution to getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -208,8 +208,14 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let nextId = startFrom;
+
+  return () => {
+    const curId = nextId;
+    nextId += 1;
+    return curId;
+  };
 }
 
 module.exports = {
